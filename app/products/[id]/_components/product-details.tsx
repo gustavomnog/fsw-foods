a59cbee2200663e974666a8489c49e09092cb212
Gustavo M Nogueira <gustavo.mnogueira95@gmail.com>
2024-06-04 21:16:24 -0300

    feat: add cart button product page

--- app/products/[id]/_components/product-details.tsx
+++ app/products/[id]/_components/product-details.tsx
@@ -47,112 +47,113 @@ export const ProductDetails = ({
 
   return (
     <div className="relative z-50 mt-[-1.5rem] rounded-tl-3xl bg-white py-5">
-      <div className="p-5">
-        {/* RESTAURANTE */}
-        <div className="flex items-center gap-[0.375rem]">
-          <div className="relative h-6 w-6">
-            <Image
-              src={product.restaurant.imageUrl}
-              alt={product.restaurant.name}
-              fill
-              className="rounded-full object-cover"
-            />
-          </div>
-          <span className="text-xs text-muted-foreground">
-            {product.restaurant.name}
-          </span>
+      {/* RESTAURANTE */}
+      <div className="flex items-center gap-[0.375rem] px-5">
+        <div className="relative h-6 w-6">
+          <Image
+            src={product.restaurant.imageUrl}
+            alt={product.restaurant.name}
+            fill
+            className="rounded-full object-cover"
+          />
         </div>
+        <span className="text-xs text-muted-foreground">
+          {product.restaurant.name}
+        </span>
+      </div>
 
-        {/* NOME DO PRODUTO */}
-        <h1 className="mb-3 mt-1 text-xl font-semibold">{product.name}</h1>
+      {/* NOME DO PRODUTO */}
+      <h1 className="mb-3 mt-1 px-5 text-xl font-semibold">{product.name}</h1>
 
-        {/* PREÇO DO PRODUTO E QUANTIDADE */}
-        <div className="flex justify-between">
-          <div>
-            <div className="flex items-center gap-2">
-              {/* PREÇO COM DESCONTO */}
-              <h2 className="text-xl font-semibold">
-                {formatCurrency(calculateProductTotalPrice(product))}
-              </h2>
+      {/* PREÇO DO PRODUTO E QUANTIDADE */}
+      <div className="flex justify-between px-5">
+        <div>
+          <div className="flex items-center gap-2">
+            {/* PREÇO COM DESCONTO */}
+            <h2 className="text-xl font-semibold">
+              {formatCurrency(calculateProductTotalPrice(product))}
+            </h2>
 
-              {product.discountPercentage > 0 && (
-                <DiscountBadge product={product} />
-              )}
-            </div>
-            {/* PREÇO ORIGINAL */}
             {product.discountPercentage > 0 && (
-              <p className="text-sm text-muted-foreground">
-                De: {formatCurrency(Number(product.price))}
-              </p>
+              <DiscountBadge product={product} />
             )}
           </div>
-
-          {/* QUANTIDADE */}
-          <div className="flex items-center gap-3 text-center">
-            <Button
-              size="icon"
-              variant="ghost"
-              className="border border-solid border-muted-foreground"
-              onClick={handleDecreaseQuantityClick}
-            >
-              <ChevronLeftIcon />
-            </Button>
-
-            <span className="w-4">{quantity}</span>
-
-            <Button
-              size="icon"
-              className="border border-solid border-muted-foreground"
-              onClick={handleIncreaseQuantityClick}
-            >
-              <ChevronRightIcon />
-            </Button>
-          </div>
-        </div>
-
-        {/* DADOS DA ENTREGA */}
-        <Card className="mt-6 flex justify-around py-3">
-          {/* VALOR */}
-          <div className="flex flex-col items-center">
-            <div className="flex items-center gap-1 text-muted-foreground">
-              <span className="text-xs">Entrega</span>
-              <BikeIcon size={14} />
-            </div>
-
-            {Number(product.restaurant.deliveryFee) > 0 ? (
-              <p className="text-xs font-semibold">
-                {formatCurrency(Number(product.restaurant.deliveryFee))}
-              </p>
-            ) : (
-              <p className="text-xs font-semibold">Grátis</p>
-            )}
-          </div>
-
-          {/* TEMPO */}
-          <div className="flex flex-col items-center">
-            <div className="flex items-center gap-1 text-muted-foreground">
-              <span className="text-xs">Entrega</span>
-              <TimerIcon size={14} />
-            </div>
-
-            <p className="text-xs font-semibold">
-              {product.restaurant.deliveryTimeMinutes} min
+          {/* PREÇO ORIGINAL */}
+          {product.discountPercentage > 0 && (
+            <p className="text-sm text-muted-foreground">
+              De: {formatCurrency(Number(product.price))}
             </p>
-          </div>
-        </Card>
-
-        {/* SOBRE */}
-        <div className="mt-6 space-y-3">
-          <h3 className="font-semibold">Sobre</h3>
-          <p className="text-sm text-muted-foreground">{product.description}</p>
+          )}
         </div>
 
-        <div className="mt-6 space-y-3">
-          <h3 className="font-semibold">Sucos</h3>
+        {/* QUANTIDADE */}
+        <div className="flex items-center gap-3 text-center">
+          <Button
+            size="icon"
+            variant="ghost"
+            className="border border-solid border-muted-foreground"
+            onClick={handleDecreaseQuantityClick}
+          >
+            <ChevronLeftIcon />
+          </Button>
+
+          <span className="w-4">{quantity}</span>
+
+          <Button
+            size="icon"
+            className="border border-solid border-muted-foreground"
+            onClick={handleIncreaseQuantityClick}
+          >
+            <ChevronRightIcon />
+          </Button>
         </div>
       </div>
 
-      <ProductList products={complementaryProducts} />
+      {/* DADOS DA ENTREGA */}
+      <Card className="mx-5 mt-6 flex justify-around py-3">
+        {/* VALOR */}
+        <div className="flex flex-col items-center">
+          <div className="flex items-center gap-1 text-muted-foreground">
+            <span className="text-xs">Entrega</span>
+            <BikeIcon size={14} />
+          </div>
+
+          {Number(product.restaurant.deliveryFee) > 0 ? (
+            <p className="text-xs font-semibold">
+              {formatCurrency(Number(product.restaurant.deliveryFee))}
+            </p>
+          ) : (
+            <p className="text-xs font-semibold">Grátis</p>
+          )}
+        </div>
+
+        {/* TEMPO */}
+        <div className="flex flex-col items-center">
+          <div className="flex items-center gap-1 text-muted-foreground">
+            <span className="text-xs">Entrega</span>
+            <TimerIcon size={14} />
+          </div>
+
+          <p className="text-xs font-semibold">
+            {product.restaurant.deliveryTimeMinutes} min
+          </p>
+        </div>
+      </Card>
+
+      {/* SOBRE */}
+      <div className="mt-6 space-y-3 px-5">
+        <h3 className="font-semibold">Sobre</h3>
+        <p className="text-sm text-muted-foreground">{product.description}</p>
+      </div>
+
+      <div className="mt-6 space-y-3">
+        <h3 className="px-5 font-semibold">Sucos</h3>
+        <ProductList products={complementaryProducts} />
+      </div>
+
+      <div className="mt-6 px-5">
+        <Button className="w-full font-semibold">Adicionar á sacola</Button>
+      </div>
     </div>
   );
 };
