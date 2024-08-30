93c0f35d36945df4dec1a17d4c8e3b932d18de92
Gustavo M Nogueira <gustavo.mnogueira95@gmail.com>
2024-08-30 18:53:44 -0300

    feat: move restaurant list to a client component

--- app/_components/promo-banner.tsx
+++ app/_components/promo-banner.tsx
@@ -2,6 +2,7 @@ import Image, { ImageProps } from "next/image";
 
 export const PromoBanner = (props: ImageProps) => {
   return (
+    // eslint-disable-next-line jsx-a11y/alt-text
     <Image
       width={0}
       height={0}
---
+++ app/restaurants/_components/restaurants.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { notFound, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { searchForrestaurants } from "../_actions/search";
+import { Restaurant } from "@prisma/client";
+import { RestaurantItem } from "../../_components/restaurant-item";
+import { Header } from "../../_components/header";
+
+const Restaurants = () => {
+  const searchParams = useSearchParams();
+  const [restaurants, setRestaurants] = useState<Restaurant[] | null>(null);
+
+  const searchFor = searchParams.get("search");
+
+  useEffect(() => {
+    const fetchRestaurants = async () => {
+      if (!searchFor) return;
+      const foundRestaurants = await searchForrestaurants(searchFor);
+      setRestaurants(foundRestaurants);
+    };
+
+    fetchRestaurants();
+  }, [searchFor]);
+
+  if (!searchFor) {
+    return notFound();
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="px-5 py-6">
+        <h2 className="tex-lg mb-6 font-semibold">Restaurantes Recomendados</h2>
+
+        {restaurants && restaurants.length === 0 && (
+          <div className="w-full text-center">
+            <span>Nenhum restaurante encontrado!</span>
+          </div>
+        )}
+        <div className="flex w-full flex-col gap-6">
+          {restaurants?.map((restaurant) => (
+            <RestaurantItem
+              key={restaurant.id}
+              restaurant={restaurant}
+              className="min-w-full max-w-full"
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Restaurants;
